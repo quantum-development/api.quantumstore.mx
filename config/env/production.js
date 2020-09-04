484ebebee20cf63369b5fe8a5438cfd97735e960
Alexander Angulo <alex.anguloh@gmail.com>
2020-09-04 13:24:15 -0500

    Change Enviroment values

--- config/env/production.js
+++ config/env/production.js
@@ -43,7 +43,7 @@ module.exports = {
     openpay: {
       id: 'mqfufzvnqrp2nyezxoub',
       privateKey: 'sk_edfa5574382841118d0b7f7786b2862b',
-      production: true
+      production: false
     },
     app_info: {
       web: 'https://www.quantumstore.com.mx/'
@@ -51,7 +51,7 @@ module.exports = {
     reward_provider: {
       client_slug: 'quantumstore',
       url: 'https://api.qrewards.mx/',
-      demo: 0
+      demo: 1
     },
     email_provider: {
       url: 'https://api.qrewards.mx/store/notifications/'
